71102c45edaf27829a03bc70ee0acf1cc8bf64d9
Matt Langbehn <matthew.langbehn@gmail.com>
2017-10-27 16:10:09 -0500

    [WIP][DNM] Update CI [DNM][WIP]
    Signed-off-by: Matt Langbehn <matthew.langbehn@gmail.com>

--- tests/test-javac.sh
+++ tests/test-javac.sh
@@ -3,7 +3,7 @@
 # shellcheck disable=SC1090
 source "$(dirname "$0")"/../scripts/resources.sh
 
-if find . -name '*.sh' -print0 | xargs -n1 -0 shellcheck -x -s bash; then
+if find . -name '*.java' -print0 | xargs -n1 -0 javac -Xlint; then
     test_passed "$0"
 else
     test_failed "$0"
